4d5fa6bdb8add6818a389ac6f91e1ec9a77db0a8
steventucci <tuccisteven@gmail.com>
2017-01-17 01:36:03 -0500

    Added some todos

--- comp442_compilers/dfa.h
+++ comp442_compilers/dfa.h
@@ -17,7 +17,8 @@ public:
 
 private:
 	state m_start_state;
-	// TODO implement state transition has this
+	// TODO: implement state transition has this
+	// TODO: implmennt some way to track backtrackking and final tokens
 	//std::unordered_map<state, std::unordered_map<token, state>> state_transition_table;
 	std::unordered_map<int, std::unordered_map<std::string, state>> state_transition_table;
 };
