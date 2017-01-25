ff5f4e524d4360581b2df410256a2f1be0cac10d
steventucci <tuccisteven@gmail.com>
2017-01-25 16:02:27 -0500

    Added writing to file

--- comp442_compilers/main.cpp
+++ comp442_compilers/main.cpp
@@ -4,19 +4,43 @@
 using namespace std;
 
 int main() {
+
+	bool write_to_file = true;
+	ofstream output;
+	ofstream error;
+
+	if (write_to_file) {
+		output.open("output.txt");
+		error.open("errors.txt");
+	}
+
 	specification spec;
 	lexer lex(&spec);
-	// TODO: write tokens out to file
-	if (lex.set_source("SourceCode.txt")) {
-		while (lex.has_more_tokens()) {
-			token t = lex.next_token();
-			cout << "lexeme: " << t.lexeme << ", "
-				<< "type: " << t.type << ", "
-				<< "line number: " << t.token_line << endl;
+	lex.set_source("SourceCode.txt");
+
+	while (lex.has_more_tokens()) {
+		token t = lex.next_token();
+		string token_output = "lexeme: " + t.lexeme +  "\t\ttype: " + to_string(t.type) + "\t\tline number: " + to_string(t.token_line) + "\n";
+
+		if (write_to_file) {
+			// write to output/error file
+			if (t.type == token_type::error_token) {
+				error << token_output;
+			} else {
+				output << token_output;
+			}
+		} else {
+			// Write to console
+			cout << token_output;
 		}
-	} else {
-		cout << "Source code not found" << endl;
 	}
+
+
+	if (write_to_file) {
+		output.close();
+		error.close();
+	}
+	
 
 
 	
